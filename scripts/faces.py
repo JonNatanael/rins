3ddef839a0e7199e9b1a062a4b111f45abcf80d7
Peter Benko <peter.benko@gmail.com>
2015-04-23 18:49:42 +0200

    wtf 6

--- scripts/faces.py
+++ scripts/faces.py
@@ -63,8 +63,8 @@ class FaceMapper():
 
                 try:
                     listener = TransformListener()
-                    listener.waitForTransform("/map", "/camera_rgb_optical_frame", rospy.Time(0), rospy.Duration(2.0))
-                    (trans, rot) = listener.lookupTransform('/map', '/camera_rgb_optical_frame', rospy.Time(0))
+                    listener.waitForTransform("/map", "/camera_rgb_frame", rospy.Time(0), rospy.Duration(2.0))
+                    (trans, rot) = listener.lookupTransform('/map', '/camera_rgb_frame', rospy.Time(0))
                     #(trans, rot) = listener.lookupTransform('/map', '/camera_rgb_optical_frame', faces.header.stamp)
                     print trans,rot
                 except:
