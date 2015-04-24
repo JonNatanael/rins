4104c41efd6957d603f9724ff8453b0765079b06
Denis Bavcar <denis.bavcar@gmail.com>
2015-04-24 11:09:36 +0000

    spremenjen  clustering

--- scripts/clusters.py
+++ scripts/clusters.py
@@ -1,5 +1,7 @@
 
-def makeClusters(self, hits):
+def makeFaceClusters(self, hits):
+# spremenjena verzija, bo mogoce boljsi preformance
+# zdaj prejmo PoseArray, ne MarkerArray
 
     num_closest = 15 #how many must be in the desired range to be consisered a cluster
     spread = 0.35 #how small must the cluster be
@@ -7,27 +9,26 @@ def makeClusters(self, hits):
 
     #find all contenders. raw is a list of tuples: cluster(x_center, y_center, max_distance_from_center)
     raw = []
-    for marker in hits.markers:
+    for marker in hits.poses:
 
-        position = marker.pose
         #sorted_by_dist = sorted(hits, key=self.dist(hits.))
         points_in_range = 0
         max_dist = 0
+        points_cluster_xy = []
 
-        for contender in hits.markers: #O(n^2), yay!
+        for contender in hits.poses: #O(n^2), yay!
 
             if contender == marker: #if it's our center ignore it
                 continue
 
             #get the distance from the center point
-            dst = dist(marker.pose.point.x, marker.pose.point.y, contender.pose.point.x, contender.pose.point.y)
+            dst = dist(marker.point.x, marker.point.y, contender.point.x, contender.point.y)
             if dst < spread: #if it's inside the spread add to the cluster size counter 
                 points_in_range += 1
-                if dst > max_dist: #adjust the max dist
-                    max_dist = dst
+                points_cluster_xy.append((contender.point.x, contender.point.y))
 
         if points_in_range > num_closest: #if we had enough nearby points to consider this a cluster
-            raw.append((marker.pose.point.x, marker.pose.point.y, max_dist)) #add our initial center with the furthest point inside the cloud info intact
+            raw.append((marker.point.x, marker.point.y, points_in_range, points_cluster_xy)) #add our initial center and number of points in cloud
 
     #we should now have a list of all cluster centers stored in raw[]
     #we need to define our logic for what the best clusters are. In this instance I assume the tightest one as in lowest max_dist
@@ -51,5 +52,4 @@ def makeClusters(self, hits):
             clusters.append(sorted_raw[i])
 
     return clusters #returns a list of tuples: tup(cluster_center.x, cluster_center.y, cluster_sperad/2)
-
 
