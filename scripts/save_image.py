cff642436dbb4ba727590454a7218bea41e2c2f3
team beta <beta@rins.si>
2015-06-03 18:34:59 +0200

    Adding image saver + some images

--- scripts/save_image.py
+++ scripts/save_image.py
@@ -15,24 +15,51 @@ from visualization_msgs.msg import Marker, MarkerArray
 from image_geometry import PinholeCameraModel
 from geometry_msgs.msg import Point, Vector3, PointStamped
 from tf import TransformListener
+import random
+import string
 
 
-def image_callback(self, image, camera):
-#def image_callback(self, image):
-	#we need to bringup minimal.launch
-	#we need to bringup 3dsensor.launch
-	#amcl_demo.launch
-	#localizer/localizer
-	#rosrun mas_server map_server map/map.yaml
+class ImageSaver():
 
-	camera_topic = rospy.get_param('~camera_topic', '/camera/rgb/camera_info')
-	self.camera_sub = message_filters.Subscriber(camera_topic, CameraInfo)
+	def image_callback(self,image, camera):
+	#def image_callback(self, image):
+		#we need to bringup minimal.launch
+		#we need to bringup 3dsensor.launch
+		#amcl_demo.launch
+		#localizer/localizer
+		#rosrun mas_server map_server map/map.yaml
 
-	try:
-		cv_image = self.bridge.imgmsg_to_cv2(image, "bgr8")
-	except CvBridgeError, e:
-		print e
+		#print image
+		N = 15
 
+		try:
+			cv_image = self.bridge.imgmsg_to_cv2(image, "bgr8")	
+			#print cv_image		
+			cv2.imshow("Image window", cv_image)
+			key = cv2.waitKey(3)
+			if key==10:
+				name = ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(N))
+				name ='images/'+name+'.jpg'
+				print name
+				cv2.imwrite(name, cv_image)
+
+		except CvBridgeError, e:
+			print e
+
+
+	def __init__(self):
+		cv2.namedWindow("Image window", 1)
+		self.bridge = CvBridge()
+
+		image_topic = rospy.get_param('~image_topic', '/camera/rgb/image_color')
+		camera_topic = rospy.get_param('~camera_topic', '/camera/rgb/camera_info')		
+
+		
+
+		self.image_sub = message_filters.Subscriber(image_topic, Image)
+		self.camera_sub = message_filters.Subscriber(camera_topic, CameraInfo)
+		self.joined_sub = message_filters.TimeSynchronizer([self.image_sub, self.camera_sub], 10)
+		self.joined_sub.registerCallback(self.image_callback)
 
 
 
@@ -41,9 +68,10 @@ def image_callback(self, image, camera):
 # Main function.    
 if __name__ == '__main__':
 
+		print "Starting.."
 		rospy.init_node('image_saver')
 		try:
-			#fd = CyllinderDetector()
+			fd = ImageSaver()
 			rospy.spin()	
 		except rospy.ROSInterruptException:
 			print "Shutting down"
