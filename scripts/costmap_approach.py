db187887d068a9987fa1924a86517fc592f9db11
team beta <beta@rins.com>
2015-06-07 12:08:05 +0200

    Costmap combines properly, prepared to resize the map

--- scripts/costmap_approach.py
+++ scripts/costmap_approach.py
@@ -19,7 +19,8 @@ from tf import TransformListener
 class CostmapApproach():
 
 	width = -1
-	height = -1
+	height = -1 
+	base_costmap = np.array([])
 
 	def costmap_callback(self, costmap):
 		self.width = costmap.info.width
@@ -35,19 +36,41 @@ class CostmapApproach():
 		np_arr = np.reshape(np_arr, (self.height, self.width))
 		cv_image = np.asarray(np_arr, np.uint8)
 		cv_image = cv2.flip(cv_image,0)
-		cv_image = (255-cv_image)
+		#cv_image = (255-cv_image)
+
+		self.base_costmap = cv_image
 		#print costmap.data
 		#print np_arr
-		cv2.imshow("Initial costmap", cv_image)
-		cv2.waitKey(0)
+		#cv2.imshow("Initial costmap", cv_image)
+		#cv2.waitKey(50)
 
 		print "Got costmap", self.width, self.height
 
 	def update_callback(self, update):
 		if self.width < 0 or self.height < 0:
+			print "Costmap update blocked"
 			return None
 
-		print "Got update"
+		np_arr = []
+		for x in update.data:
+			if x < 0:
+				np_arr.append(0)
+			else:
+				np_arr.append(x)
+
+		np_arr = np.reshape(np_arr, (update.height, update.width))
+		cv_image = np.asarray(np_arr, np.uint8)
+		cv_image = cv2.flip(cv_image,0)
+		#cv_image = (255-cv_image)
+
+		img = self.base_costmap
+		y = self.height - update.y - update.height
+		img[ y : y+update.height, update.x:update.x+update.width] = cv_image
+
+		#print costmap.data
+		#print np_arr
+		cv2.imshow("Costmap update", img)
+		cv2.waitKey(3)
 
 	def __init__(self):
 		costmap_topic = rospy.get_param('~costmap_topic', '/move_base/global_costmap/costmap')
